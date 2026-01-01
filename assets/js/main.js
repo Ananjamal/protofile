fca13f1f081d5d2fbf4b7c481972cf56258c0797
anan <ananomar51@gmail.com>
2026-01-01 12:16:42 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,21 +52,21 @@ var typingEffect = new Typed(".typedText", {
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
 const sr = ScrollReveal({
   origin: "top",
-  distance: "80px",
-  duration: 2000,
+  distance: "60px",
+  duration: 800,
   reset: true,
 });
 
 /* -- HOME -- */
 sr.reveal(".featured-text-card", {});
-sr.reveal(".featured-name", { delay: 100 });
-sr.reveal(".featured-text-info", { delay: 200 });
-sr.reveal(".featured-text-btn", { delay: 200 });
-sr.reveal(".social_icons", { delay: 200 });
-sr.reveal(".featured-image", { delay: 300 });
+sr.reveal(".featured-name", { delay: 50 });
+sr.reveal(".featured-text-info", { delay: 100 });
+sr.reveal(".featured-text-btn", { delay: 150 });
+sr.reveal(".social_icons", { delay: 150 });
+sr.reveal(".featured-image", { delay: 200 });
 
 /* -- PROJECT BOX -- */
-sr.reveal(".project-box", { interval: 200 });
+sr.reveal(".project-box", { interval: 100 });
 
 /* -- HEADINGS -- */
 sr.reveal(".top-header", {});
@@ -76,24 +76,24 @@ sr.reveal(".top-header", {});
 /* -- ABOUT INFO & CONTACT INFO -- */
 const srLeft = ScrollReveal({
   origin: "left",
-  distance: "80px",
-  duration: 2000,
+  distance: "60px",
+  duration: 800,
   reset: true,
 });
 
-srLeft.reveal(".about-info", { delay: 100 });
-srLeft.reveal(".contact-info", { delay: 100 });
+srLeft.reveal(".about-info", { delay: 50 });
+srLeft.reveal(".contact-info", { delay: 50 });
 
 /* -- ABOUT SKILLS & FORM BOX -- */
 const srRight = ScrollReveal({
   origin: "right",
-  distance: "80px",
-  duration: 2000,
+  distance: "60px",
+  duration: 800,
   reset: true,
 });
 
-srRight.reveal(".skills-box", { delay: 100 });
-srRight.reveal(".form-control", { delay: 100 });
+srRight.reveal(".skills-box", { delay: 50 });
+srRight.reveal(".form-control", { delay: 50 });
 
 /* ----- CHANGE ACTIVE LINK ----- */
 
